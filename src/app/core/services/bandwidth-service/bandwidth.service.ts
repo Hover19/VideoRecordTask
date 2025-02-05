7e52eb3e9084a58322a7d92d6ce663ff2ff6894e
Hover19 <klaskintrade@gmail.com>
2025-02-05 09:34:47 +0200

    add bandwidth detection service

---
+++ src/app/core/services/bandwidth-service/bandwidth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class BandwidthService {
+  async checkBandwidth(): Promise<number> {
+    if ('connection' in navigator) {
+      const connection = (navigator as any).connection;
+      if (connection && connection.downlink) {
+        return Promise.resolve(connection.downlink);
+      }
+    }
+    return Promise.resolve(3);
+  }
+}
