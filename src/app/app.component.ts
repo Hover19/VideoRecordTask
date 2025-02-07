7485187f03634ab2b84cda469e11c0b31daa88f3
Hover19 <klaskintrade@gmail.com>
2025-02-07 13:52:04 +0200

    add spinner on loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,16 @@ import { Store } from '@ngxs/store';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  public isLoading: boolean = true;
+
   constructor(private bandwidthService: BandwidthService) {}
 
   async ngOnInit() {
     const bandwidth = await this.bandwidthService.checkBandwidth();
     console.log('Detected bandwidth:', bandwidth, 'Mbps');
+  }
+
+  onCameraLoaded(status: boolean) {
+    this.isLoading = !status;
   }
 }
